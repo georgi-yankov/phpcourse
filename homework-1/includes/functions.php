d7db04234b86edceb315da6aba2963f5287317ac
Georgi Yankov <georgi.st.yankov@gmail.com>
2013-09-25 14:05:29 +0300

    function safeInput() is updated

--- homework-1/includes/functions.php
+++ homework-1/includes/functions.php
@@ -3,7 +3,7 @@
 function safeInput($string) {
 	$string = trim($string);
         $string = str_replace('!', '', $string);
-	$string = strip_tags($string);
+        $string = stripslashes($string);
 	$string = htmlspecialchars($string);
 	return $string;
 }
