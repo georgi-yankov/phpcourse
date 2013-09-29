2d7459af24ec21394fc54b50fa007738ced0b023
Georgi Yankov <georgi.st.yankov@gmail.com>
2013-09-29 11:10:48 +0300

    function downloadFile() - added
    Revised the code for download a file. Used a function instead.

--- homework-2/processing/download.php
+++ homework-2/processing/download.php
@@ -16,10 +16,4 @@ $fileToDownload = $postId . '.jpg';
 $dir = realpath('../users/user-' . $userId);
 $fullPath = $dir . DIRECTORY_SEPARATOR . $postId . '.jpg';
 
-if (!file_exists($fullPath)) {
-    die('File not found!');
-}
-
-header('Content-Type: application/octet-stream');
-header('Content-disposition: inline; filename="' . $fileToDownload . '"');
-print file_get_contents($fullPath);
+downloadFile($fullPath);
