3a0c45e46e790ba28b9fc7892bd1e8cf19e97bb2
Georgi Yankov <georgi.st.yankov@gmail.com>
2013-09-30 11:57:03 +0300

    placed default username and password

--- homework-2/includes/header.php
+++ homework-2/includes/header.php
@@ -27,8 +27,8 @@ if (existLoggedUser()) {
                 <?php } else { ?>
                     <div id="login-form">
                         <form method="POST" action="processing/check-user.php" role="form">
-                            <input type="text" name="username" placeholder="username" required />
-                            <input type="password" name="password" placeholder="password" required />
+                            <input type="text" name="username" placeholder="username" value="user" required />
+                            <input type="password" name="password" placeholder="password" value="qwerty" required />
                             <input type="submit" name="user-action" value="Login" />
                         </form>
                     </div>
